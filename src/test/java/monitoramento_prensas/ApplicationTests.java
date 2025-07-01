33cf70342c366da853a5b466d8f828fe67dafd97
Carlos Vieira <carloscelpv@outlook.com>
2025-06-30 22:56:58 -0300

    Initial commit

---
+++ src/test/java/monitoramento_prensas/ApplicationTests.java
@@ -0,0 +1,13 @@
+package monitoramento_prensas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
