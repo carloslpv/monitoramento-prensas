7ab160dc35788e1ad26172e8ba68b9ec558afc99
Carlos Vieira <carloscelpv@outlook.com>
2025-07-02 22:45:41 -0300

    feat: Cria endpoint getTelemetria

--- src/main/java/monitoramento_prensas/controllers/TelemetriaController.java
+++ src/main/java/monitoramento_prensas/controllers/TelemetriaController.java
@@ -1,14 +1,12 @@
 package monitoramento_prensas.controllers;
 
+import monitoramento_prensas.exceptions.ObjetoNaoEncontradoException;
 import monitoramento_prensas.exceptions.PersistenceException;
 import monitoramento_prensas.models.dtos.TelemetriaDTO;
 import monitoramento_prensas.services.TelemetriaService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Controller de Telemetria
@@ -39,4 +37,19 @@ public class TelemetriaController {
                     .body("Algo deu errado ao tentar criar o cadastro da telemetria. Tente novamente");
         }
     }
+
+    @GetMapping
+    public ResponseEntity<Object> getTelemetria(@RequestParam Integer idTelemetria) {
+        try {
+            final TelemetriaDTO dto = this.telemetriaService.getTelemetriaDTO(idTelemetria);
+            return ResponseEntity.ok(dto);
+        } catch (ObjetoNaoEncontradoException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Algo deu errado ao tentar buscar o cadastro da telemetria. Tente novamente");
+        }
+    }
+
 }
