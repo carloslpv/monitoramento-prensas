c7f86e54e634d8ecae5ed313ff6a188e6795ce6d
Carlos Vieira <carloscelpv@outlook.com>
2025-07-02 23:42:27 -0300

    feat: Cria classe HistoricoManutencaoController

---
+++ src/main/java/monitoramento_prensas/controllers/HistoricoManutencaoController.java
@@ -0,0 +1,42 @@
+package monitoramento_prensas.controllers;
+
+import monitoramento_prensas.exceptions.PersistenceException;
+import monitoramento_prensas.models.dtos.HistoricoManutencaoDTO;
+import monitoramento_prensas.services.HistoricoManutencaoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller de HistoricoManutencao
+ *
+ * @author Carlos Vieira
+ * @since 02/07/2025
+ */
+@RestController
+@RequestMapping("/historico-manutencao")
+public class HistoricoManutencaoController {
+
+    private HistoricoManutencaoService historicoManutencaoService;
+
+    public HistoricoManutencaoController(HistoricoManutencaoService historicoManutencaoService) {
+        this.historicoManutencaoService = historicoManutencaoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createMaquina(@RequestBody HistoricoManutencaoDTO dto) {
+        try {
+            this.historicoManutencaoService.createHistoricoManutencao(dto);
+            return ResponseEntity.ok("Máquina cadastrada com sucesso.");
+        } catch (PersistenceException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Algo deu errado ao tentar criar o cadastro da máquina. Tente novamente");
+        }
+    }
+}
