a81e9b28a1f703a758a970ec174ca2e2654c4b74
Vinicius Rinas Begnini <beguim1313@gmail.com>
2025-07-02 23:18:39 -0300

    feat:cria classe HistoricoFalhasRepository

--- src/main/java/monitoramento_prensas/repositories/HistoricoFalhasRepository.java
+++ src/main/java/monitoramento_prensas/repositories/HistoricoFalhasRepository.java
@@ -1,4 +1,14 @@
 package monitoramento_prensas.repositories;
 
-public interface HistoricoFalhasRepository {
+import monitoramento_prensas.models.HistoricoFalhas;
+import monitoramento_prensas.models.Maquina;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Repository de {@link HistoricoFalhas}
+ *
+ * @author Vinicius Begnini
+ * @since 02/07/2025
+ */
+public interface HistoricoFalhasRepository extends JpaRepository<HistoricoFalhas, Integer> {
 }
