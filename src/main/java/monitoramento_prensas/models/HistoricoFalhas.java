53830c4cd38bbdbe03b21c2d5cc63d88091b3c97
Vinicius Rinas Begnini <beguim1313@gmail.com>
2025-07-02 23:17:51 -0300

    feat: cria metodo todto

--- src/main/java/monitoramento_prensas/models/HistoricoFalhas.java
+++ src/main/java/monitoramento_prensas/models/HistoricoFalhas.java
@@ -5,9 +5,17 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import monitoramento_prensas.models.dtos.HistoricoFalhasDTO;
+import monitoramento_prensas.models.dtos.MaquinaDTO;
 
 import java.time.LocalDateTime;
 
+/**
+ * Entidade de HistoricoFalhas.
+ *
+ * @author Vinicius Begnini
+ * @since 01/07/2025
+ */
 @Entity
 @Table(name = "historico_falhas")
 @AllArgsConstructor
@@ -29,4 +37,17 @@ public class HistoricoFalhas {
     @Column(name = "data_falha")
     private LocalDateTime dataFalha;
 
+    /**
+     * Retorna um objeto {@link monitoramento_prensas.models.dtos.HistoricoFalhasDTO}
+     *
+     * @return
+     */
+    public HistoricoFalhasDTO toDTO() {
+        return new HistoricoFalhasDTO(
+                this.id,
+                this.maquina != null ? this.maquina.getId() : null,
+                this.tipoFalha,
+                this.dataFalha
+        );
+    }
 }
